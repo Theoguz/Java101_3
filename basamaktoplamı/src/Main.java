04bf3e39243e10b144523ad48afdee345faddbd1
Oguzz <oguzaslan_203@hotmail.com>
2023-03-01 22:03:08 +0300

    Bir sayının basamak sayılarının toplamını hesaplayan program

---
+++ basamaktoplamı/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz: ");
+        int number = scanner.nextInt();
+        int result = 0;
+        int basvalue;
+        int sayac = 0;
+        int tempnumber = number;
+
+        while (number != 0) {
+            number = number / 10;
+            sayac++;
+        }
+        number = tempnumber;
+        System.out.println(number);
+        while (number != 0) {
+            basvalue = number % 10;
+            result += basvalue;
+            number = number / 10;
+        }
+        System.out.println("Basamakların toplamı: " + result);
+    }
+}

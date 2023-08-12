a9389710bd54f1fae17d82686dab31d4895f4ec6
Oguzz <oguzaslan_203@hotmail.com>
2023-08-12 20:40:39 +0300

    harmoniksayi

---
+++ harmonikseri/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sayi giriniz: ");
+        int n = scanner.nextInt();
+        double result = 0;
+        for (int i = 1; i <= n; i++){
+
+        result +=(1.0/i);
+        }
+        System.out.println(result);
+    }
+}
